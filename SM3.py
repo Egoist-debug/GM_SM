030ddd558b4763db8ed4382df4ee5bb347547154
Zhang yuhao <sbisyou38@outlook.com>
2023-11-06 18:49:37 +0800

    update

--- SM3.py
+++ SM3.py
@@ -1,120 +1,126 @@
-# 数字格式转化
-def int2str( num ) :
-    out = ""
-    out = out + chr( num//65536 ) + chr( (num%65536)//256 ) + chr( num%256 )
-    return out
+'''
+SM3
+'''
+#初始值
+IV = [0x7380166F, 0x4914B2B9, 0x172442D7, 0xDA8A0600, 0xA96F30BC, 0x163138AA, 0xE38DEE4D, 0xB0FB0E4E]
 
-def str2hex( string ):
-    out = ""
-    for i in range ( 0 , len(string) ):
-        out = out + " " + hex(ord( string[i] ))
-    return out
+#常量
+T = [0x79cc4519, 0x7a879d8a]
 
-# 字符串异或运算，后面分别是字符串与、或、非运算
-def strxor( message , key , len ):
-    out = ""
-    for i in range ( 0 , len ):
-        ch = ord(message[i]) ^ ord(key[i])
-        out = out + chr(ch)
-    return out
+def ROL(X,i):
+    i = i % 32
+    return ((X<<i)&0xFFFFFFFF) | ((X&0xFFFFFFFF)>>(32-i))
 
-def strand( message , key , len ):
-    out = ""
-    for i in range ( 0 , len ):
-        ch = ord(message[i]) & ord(key[i])
-        out = out + chr(ch)
-    return out
+#布尔函数
+def FF(X,Y,Z,j):
+    if j>=0 and j<=15:
+        return X ^ Y ^ Z
+    else:
+        return ((X & Y) | (X & Z) | (Y & Z))
+def GG(X,Y,Z,j):
+    if j>=0 and j<=15:
+        return X ^ Y ^ Z
+    else:
+        return ((X & Y) | (~X & Z))
 
-def stror( message , key , len ):
-    out = ""
-    for i in range ( 0 , len ):
-        ch = ord(message[i]) | ord(key[i])
-        out = out + chr(ch)
-    return out
+#置换函数
+def P0(X):
+    return X^ROL(X,9)^ROL(X,17)
+def P1(X):
+    return X^ROL(X,15)^ROL(X,23)
+    #常量在0<=j<=15取0x79cc4519，在16<=j<=63取0x7a879d8a。
+def T_(j):
+    if j>=0 and j<=15:
+        return T[0]
+    else:
+        return T[1]
 
-def strnot( string , len ) :
-    out = ""
-    for i in range ( 0 , len ) :
-        ch = ~ord(string[i])
-        out = out + chr(ch)
-    return out
 
-# 字符串按比特向左移位
-def strldc( string , bit ):
-    byte = bit // 8
-    bit = bit % 8
-    out = ""
-    if bit == 0 :
-        out = string[byte:] + string[:byte]
-    else :
-        reg = string[byte:] + string[:byte+1]
-        for i in range (0,len(reg)-1):
-            out = out + chr(((ord(reg[i])*(2**bit))+(ord(reg[i+1])//(2**(8-bit))))%256)
-        out = out[:len(string)]
-    return out
+#算法部分
+#1.填充
+def Fill(message):
+    m = bin(int(message,16))[2:]
+    if len(m) != len(message)*4:
+        m = '0'*(len(message)*4-len(m)) + m
+    l = len(m)
+    l_bin = '0'*(64-len(bin(l)[2:])) + bin(l)[2:]
+    m = m + '1'
+    m = m + '0'*(448-len(m)%512) + l_bin
+    m = hex(int(m,2))[2:]
+    print("填充后的消息为:",m)
+    return m
 
-# 4字节模加运算
-def stradd_4( str1 , str2 ) :
-    out = ""
-    num1 = ord(str1[0])*16777216+ord(str1[1])*65536+ord(str1[2])*256+ord(str1[3])
-    num2 = ord(str2[0])*16777216+ord(str2[1])*65536+ord(str2[2])*256+ord(str2[3])
-    add = (num1 + num2)%4294967296
-    out = out + chr(add//1677216) + chr((add%1677216)//65536) + chr((add%65536)//256) + chr(add%256)
-    return out
+#2.迭代压缩
+   #分组
+def Group(m):
+    n = len(m)/128
+    M = []
+    for i in range(int(n)):
+        M.append(m[0+128*i:128+128*i])
+    return M
+   #消息扩展
+def Expand(M,n):
+    W = []
+    W_ = []
+    for j in range(16):
+        W.append(int(M[n][0+8*j:8+8*j],16))
+    for j in range(16,68):
+        W.append(P1(W[j-16]^W[j-9]^ROL(W[j-3],15))^ROL(W[j-13],7)^W[j-6])
+    for j in range(64):
+        W_.append(W[j]^W[j+4])
+    Wstr = ''
+    W_str = ''
+    for x in W:
+        Wstr += (hex(x)[2:] + ' ')
+    for x in W_:
+        W_str+= (hex(x)[2:] + ' ')
+    print("第{}个消息分组 扩展后消息：".format(n+1))
+    print("W:",Wstr)
+    print("W':",W_str)
+    return W,W_
+   #压缩函数
+def CF(V,M,i):
+    A,B,C,D,E,F,G,H = V[i]
+    W,W_ = Expand(M,i)
+    for j in range(64):
+        SS1 = ROL((ROL(A,12)+E+ROL(T_(j),j%32))%(2**32),7)
+        SS2 = SS1 ^ ROL(A,12)
+        TT1 = (FF(A,B,C,j)+D+SS2+W_[j])%(2**32)
+        TT2 = (GG(E,F,G,j)+H+SS1+W[j])%(2**32)
+        D = C
+        C = ROL(B,9)
+        B = A
+        A = TT1
+        H = G
+        G = ROL(F,19)
+        F = E
+        E = P0(TT2)
+        print("j={}:".format(j))
+        print(hex(A),hex(B),hex(C),hex(D),hex(E),hex(F),hex(G),hex(H))
+    a,b,c,d,e,f,g,h = V[i]
+    V_ = [a^A,b^B,c^C,d^D,e^E,f^F,g^G,h^H]
+    return V_
 
-def functionFF( A , B , C , j ) :
-    if j < 16 :
-        FF = strxor( A , strxor( B , C , 4 ) , 4 )
-    if j > 15 :
-        FF = stror( stror( strand(A,B,4) , strand(A,C,4) , 4 ) , strand(B,C,4) , 4)
-    return FF
+#迭代
+def Iterate(M):
+    n = len(M)
+    V = []
+    V.append(IV)
+    for i in range(n):
+        V.append(CF(V,M,i))
+    return V[n]
 
-def functionGG( A , B , C , j ) :
-    if j<16 :
-        GG = strxor( A , strxor( B , C , 4 ) , 4 )
-    if j > 15 :
-        GG = stror( strand(A,B,4) , strand(strnot(A,4),C,4) , 4 )
-    return GG
+if __name__ == '__main__':
+    message = '616263'
+    '''
+61626364616263646162636461626364616263646162636461626364616263646162636461626364616263646162636461626364616263646162636461626364
+    '''
+    m = Fill(message)   #填充后消息
+    M = Group(m)  #数据分组
+    Vn=Iterate(M) #迭代
+    result = ''
+    for x in Vn:
+        result += (hex(x)[2:]+' ')
+    print("杂凑值:",result)
 
-def functionP( string , mode ) :
-    out = ''
-    if mode == 0 :
-        out = strxor( string , strxor( strldc(string,9) , strldc(string,17) , 4 ) , 4 )
-    if mode == 1 :
-        out = strxor( string , strxor( strldc(string,15) , strldc(string,23) , 4 ) , 4 )
-    return out
 
-def functionCF( V , B ) :
-    for i in range (0,68) :
-        # 消息扩展过程
-        P = strxor( strxor( B[0:4] , B[28:32] , 4 ) , strldc( B[42:46] , 15 ) , 4)
-        Badd = strxor( P , strxor( B[40:44] , strldc(B[12:16],7) , 4 ) , 4 )
-        out1 = strxor( B[0:4] , B[16:20] , 4 )
-        out = B[0:4]
-        B = B[0:60] + Badd
-        # 状态更新过程
-        if i < 64 :
-            if i < 16 :
-                SS1 = strldc( stradd_4( strldc( V[0:4] , 12 ) , stradd_4( V[16:20] , strldc(T0_15,i%32) ) ) , 7 )
-            else :
-                SS1 = strldc( stradd_4( strldc( V[0:4] , 12 ) , stradd_4( V[16:20] , strldc(T16_63,i%32) ) ) , 7 )
-        SS2 = strxor( SS1 , strldc( V[0:4] , 12 ) , 4 )
-        TT1 = stradd_4( stradd_4( functionFF( V[0:4] , V[4:8] , V[8:12] , i ) , V[12:16] ) , stradd_4( SS2 , out1 ) )
-        TT2 = stradd_4( stradd_4( functionGG( V[16:20] , V[20:24] , V[24:28] , i ) , V[28:32] ) , stradd_4( SS1 , out ) )
-        V = strxor( V , TT1 + V[0:4] + strldc(V[4:8],9) + V[8:12] + functionP(TT2,0) + V[16:20] + strldc(V[20:24],19) + V[24:28] , 32 )
-    return V
-
-IV = "\x73\x80\x16\x6f\x49\x14\xb2\xb9\x17\x24\x42\xd7\xda\x8a\x06\x00\xa9\x6f\x30\xbc\x16\x31\x38\xaa\xe3\x8d\xee\x4d\xb0\xfb\x0e\x4e"
-T0_15 = "\x79\xcc\x45\x19"
-T16_63 = "\x7a\x87\x9d\x8a"
-plain = input( "请输入杂凑函数明文：" )
-l = int2str( len(plain)*8 )
-plain = plain + "\x80"
-k = 56 - (len(plain)%64) - 1
-plain = plain + "\x00"*k
-plain = plain + l
-print( "plain :" , str2hex(plain) )
-for i in range ( 0 , len(plain)//64-1 ) :
-    IV = functionCF( IV , plain[64*l:64*l+64] )
-hash_value = IV
-print( " hash :" , str2hex(hash_value) )
